b595c2b78fba3c870c9fd5171a5997a88b3b498d
sankalp50 <sankalps.singh.met17@itbhu.ac.in>
2021-05-08 01:10:29 +0530

    changes to seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
